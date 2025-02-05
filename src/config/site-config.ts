05ab1888c04c3e84d6b392594ea2b528187735d2
Christian John jiménez Calvo <christianjjc@gmail.com>
2025-02-05 10:22:56 -0500

    Prettier Added!

--- src/config/site-config.ts
+++ src/config/site-config.ts
@@ -1,5 +1,5 @@
 export const SiteConfig = {
   title: "CJ's Blog",
-  description: "Blog Oficial de CJ",
-  siteUrl: "blog.cjjc.pe",
+  description: 'Blog Oficial de CJ',
+  siteUrl: 'blog.cjjc.pe',
 };
--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,10 +1,10 @@
 export class Formatter {
   static formatDate(value: Date): string {
     const date = new Date(value);
-    return Intl.DateTimeFormat("es-PE", {
-      year: "numeric",
-      month: "long",
-      day: "2-digit",
+    return Intl.DateTimeFormat('es-PE', {
+      year: 'numeric',
+      month: 'long',
+      day: '2-digit',
     }).format(date);
   }
 }
