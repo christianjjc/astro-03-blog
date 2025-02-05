d8d3eac00252e693b680941d7dbecaf09cae2ef9
Christian John jiménez Calvo <christianjjc@gmail.com>
2025-02-05 13:09:11 -0500

    FIN SECCIÓN 05

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,45 @@
+// import { defineCollection, z } from 'astro:content';
+
+// const blogCollection = defineCollection({
+//   type: 'content',
+//   schema: ({ image }) =>
+//     z.object({
+//       title: z.string(),
+//       date: z.date(),
+//       description: z.string(),
+//       image: image().refine((img) => img.width < 10000, {
+//         message: 'Image width must be less than 10000px',
+//       }),
+
+//       //relation to author
+//       author: z.string(),
+
+//       //relation to tags
+//       tags: z.array(z.string()),
+//     }),
+// });
+
+// export const collections = {
+//   blog: blogCollection,
+// };
+
 import { defineCollection, z } from 'astro:content';
 
 const blogCollection = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image(),
 
-    //relation to author
-    author: z.string(),
+      // Relación
+      author: z.string(),
 
-    //relation to tags
-    tags: z.array(z.string()),
-  }),
+      // Relación
+      tags: z.array(z.string()),
+    }),
 });
 
 export const collections = {
