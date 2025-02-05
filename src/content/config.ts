dde0c3c8c3222ec0db61fd9e41c1f0e507ab0847
Christian John jiménez Calvo <christianjjc@gmail.com>
2025-02-05 16:28:40 -0500

    section-06-ep-79

--- src/content/config.ts
+++ src/content/config.ts
@@ -23,7 +23,7 @@
 //   blog: blogCollection,
 // };
 
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, reference, z } from 'astro:content';
 
 const blogCollection = defineCollection({
   type: 'content',
@@ -35,13 +35,24 @@ const blogCollection = defineCollection({
       image: image(),
 
       // Relación
-      author: z.string(),
+      // author: z.string(),
+      author: reference('author'),
 
       // Relación
       tags: z.array(z.string()),
     }),
 });
 
+const authorCollection = defineCollection({
+  type: 'data',
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
